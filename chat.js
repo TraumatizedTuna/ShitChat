6ee1364ba1125e09075fe87e963117663659eaa7
Josef Karlsson <effektgubben@gmail.com>
2023-10-13 09:48:41 +0200

    match()

--- chat.js
+++ chat.js
@@ -1,4 +1,7 @@
 const dicUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+
+let localDic = {};
+
 async function getWordInfo(word) {
     const response = await fetch(dicUrl + word.toLowerCase());
     return new Word((await response.json())[0]);
@@ -9,13 +12,18 @@ async function getWordInfo(word) {
 let patterns = {
     greeting: [
         { key: 'partOfSpeech', eq: ['exclamation', 'interjection'] },
-        { key: 'definitions._.definition', has: ['greeting'] }
+        { key: 'def', has: ['greeting'] }
     ]
 }
 
 function match(wordData) {
-    for (let p of patterns) {
-        if (p.all(wordData)) { }
+    const w = wordData.word;
+    for (let key in patterns) {
+        const p = patterns[key]
+        if (p.every(c => wordData.meanings.some(m => c.eq?.includes(m[c.key]) || c.has?.some(e => m[c.key].includes(e))))) {
+            localDic[key] ||= {};
+            localDic[key][wordData.word] = wordData;
+        }
     }
 }
 
@@ -24,6 +32,11 @@ class Word {
     constructor(data) {
         this.word = data.word;
         this.meanings = data.meanings;
-        this.partsOfSpeech = data.meanings.map(m => m.partOfSpeech);
+        this.meanings.forEach(m => m.def = m.definitions[0].definition.toLowerCase());
+        this.pos = data.meanings.map(m => m.partOfSpeech);
     }
+}
+
+function reply(msg) {
+    
 }
