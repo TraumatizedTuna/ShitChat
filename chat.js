ba89e3629a725d15ced133ffc17552cd4c0d1755
Josef Karlsson <effektgubben@gmail.com>
2023-10-16 21:28:29 +0200

    Reply 'Hi there' to greetings

--- chat.js
+++ chat.js
@@ -3,8 +3,12 @@ const dicUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 let localDic = {};
 
 async function getWordInfo(word) {
-    const response = await fetch(dicUrl + word.toLowerCase());
-    return new Word((await response.json())[0]);
+    try {
+        const response = await fetch(dicUrl + word.toLowerCase());
+        return new Word((await response.json())[0]);
+    } catch (error) {
+        return new Word({ word: word });
+    }
 }
 
 
@@ -17,26 +21,52 @@ let patterns = {
 }
 
 function match(wordData) {
-    const w = wordData.word;
     for (let key in patterns) {
         const p = patterns[key]
-        if (p.every(c => wordData.meanings.some(m => c.eq?.includes(m[c.key]) || c.has?.some(e => m[c.key].includes(e))))) {
+        //If some definition matches every condition of pattern p
+        if (p.every(c =>
+            wordData.meanings.some(m =>
+                c.eq?.includes(m[c.key]) ||
+                c.has?.some(e =>
+                    m[c.key].includes(e)
+                )
+            )
+        )) {
+            wordData.patterns.add(key);
             localDic[key] ||= {};
             localDic[key][wordData.word] = wordData;
         }
     }
+    return wordData;
 }
 
 
 class Word {
     constructor(data) {
         this.word = data.word;
-        this.meanings = data.meanings;
+        this.meanings = data.meanings || [];
         this.meanings.forEach(m => m.def = m.definitions[0].definition.toLowerCase());
-        this.pos = data.meanings.map(m => m.partOfSpeech);
+        this.pos = this.meanings.map(m => m.partOfSpeech);
+        this.patterns = new Set();
     }
 }
 
-function reply(msg) {
-    
+async function reply(msg) {
+    let words = msg.toLowerCase().split(' ').map(async w => match(await (getWordInfo(w))));
+    for (i in words) {
+        words[i] = match(await words[i]);
+    }
+    if (words.some(w => w.patterns.has('greeting'))) {
+        return 'Hi there'
+    }
+    return 'Sorry, I don\'t understand'
+}
+window.onload = function () {
+    const replyDiv = document.getElementById('reply');
+    const input = document.getElementById('input');
+    document.onkeyup = async function (ev) {
+        if (ev.key === 'Enter') {
+            replyDiv.innerHTML = await reply(input.value)
+        }
+    }
 }
